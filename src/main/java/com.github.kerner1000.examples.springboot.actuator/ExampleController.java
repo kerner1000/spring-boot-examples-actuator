c6cc3a93968898decdaf48027c4b58697544175c
Alexander Kerner <alexkerner1000@gmail.com>
2022-04-04 15:58:07 +0200

    Fixes

--- src/main/java/com.github.kerner1000.examples.springboot.actuator/ExampleController.java
+++ src/main/java/com.github.kerner1000.examples.springboot.actuator/ExampleController.java
@@ -1,13 +1,17 @@
 package com.github.kerner1000.examples.springboot.actuator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("hello")
 @RestController
-public class ExampleService {
+public class ExampleController {
 
     /**
-     * Dummy endpoint to keep the service running. It would shut down immediately after starting, otherwise.
+     * Dummy endpoint to keep the service running.
+     * It would shut down immediately after starting otherwise.
+     *
      * @return an arbitrary example string
      */
     @GetMapping(value="example", produces = "application/json")
